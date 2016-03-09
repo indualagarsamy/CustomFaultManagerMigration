665f98190e6e3de872c9c27b2ad6cd8e79b3bd2c
Pete <peter.giles@particular.net>
2016-03-09 11:03:13 +1100

    Behaviour  method
    Behaviour written but looking through NSB v5 there's quite a bit of
    logic around deciding if SerializationFailedForMessage and
    ProcessingAlwaysFailsForMessage.

---
+++ Version_6/CustomErrorHandlingBehaviour.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Pipeline;
+
+
+class CustomErrorHandlingBehaviour : Behavior<ITransportReceiveContext>
+{
+    private readonly CriticalError _criticalError;
+
+    public CustomErrorHandlingBehaviour(CriticalError criticalError)
+    {
+        _criticalError = criticalError;
+    }
+
+    public override async Task Invoke(ITransportReceiveContext context, Func<Task> next)
+    {
+        var message = context.Message;
+
+        try
+        {
+            await next().ConfigureAwait(false);
+        }
+        catch (MessageDeserializationException)
+        {
+            Console.WriteLine("CustomFaultManager - SerializationFailedForMessage");
+        }
+        catch (SerializationException)
+        {
+            Console.WriteLine("CustomFaultManager - SerializationFailedForMessage");
+        }
+        catch (InvalidOperationException)
+        {
+            Console.WriteLine("CustomFaultManager - SerializationFailedForMessage");
+            Console.WriteLine("CustomFaultManager - ProcessingAlwaysFailsForMessage");
+        }
+        catch (Exception exception)
+        {
+            try
+            {
+                Console.WriteLine(
+                    $"MyOwnFaultManager - Processing always fails for message '{message.MessageId}' due to an exception:",
+                    exception);
+            }
+            catch (Exception ex)
+            {
+                _criticalError.Raise("Failed to process Custom Policy for errors", ex);
+                throw;
+            }
+        }
+    }
+}
+
+
+class NewMessageProcessingPipelineStep : RegisterStep
+{
+    public NewMessageProcessingPipelineStep()
+        : base("ForwardMessagesToSqlTransport", typeof(CustomErrorHandlingBehaviour), "Adds custom error behavior to pipeline")
+    {
+        InsertAfterIfExists("FirstLevelRetries");
+        InsertAfterIfExists("SecondLevelRetries");
+        InsertAfterIfExists("MoveFaultsToErrorQueue");
+    }
+}
+
+class RegisterCustomErrorHandling : INeedInitialization
+{
+    public void Customize(EndpointConfiguration configuration)
+    {
+        configuration.Pipeline.Register<NewMessageProcessingPipelineStep>();
+    }
+}
--- Version_6/InitializeCustomFaultHandlingBehavior.cs
+++ Version_6/InitializeCustomFaultHandlingBehavior.cs
@@ -1,11 +1,11 @@
-﻿using NServiceBus;
+﻿//using NServiceBus;
 
 
-public class InitializeCustomFaultHandlingBehavior : INeedInitialization
-{
-    public void Customize(EndpointConfiguration busConfiguration)
-    {
-        busConfiguration.Pipeline.Replace("MoveFaultsToErrorQueueBehavior", typeof(CustomErrorHandlingBehavior), "Custom policy for handling messages instead of default SLR action");
-    }
-}
+//public class InitializeCustomFaultHandlingBehavior : INeedInitialization
+//{
+//    public void Customize(EndpointConfiguration busConfiguration)
+//    {
+//        busConfiguration.Pipeline.Replace("MoveFaultsToErrorQueueBehavior", typeof(CustomErrorHandlingBehavior), "Custom policy for handling messages instead of default SLR action");
+//    }
+//}
 
